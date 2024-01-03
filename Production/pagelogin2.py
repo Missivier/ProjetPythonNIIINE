37ef5e00892f097366db51d8c5001b421ae07f09
Valouze <valentin.ladrait@gmail.com>
2024-01-03 16:06:04 +0100

    modif

--- Production/pagelogin2.py
+++ Production/pagelogin2.py
@@ -1,8 +1,9 @@
 import tkinter as tk
+from tkinter import ttk
+import tkinter.ttk as ttk
 from tkinter import messagebox
-
-
-
+################################################################################################
+# page de connexion
 def page_login(root):
 
     def login():
@@ -14,12 +15,10 @@ def page_login(root):
         else:
             messagebox.showerror("Erreur de connexion", "Nom d'utilisateur ou mot de passe incorrect")
 
-#enleve le login
+    # enleve le login
     def hide_login_page():
         login_frame.place_forget()
         page_of(root)
-
-
 
     login_frame = tk.Frame(root)
     login_frame.place(relx=0.5, rely=0.5, anchor="center")
@@ -35,16 +34,40 @@ def page_login(root):
     entry_username.grid(row=0, column=1, padx=10, pady=10)
     entry_password.grid(row=1, column=1, padx=10, pady=10)
     button_login.grid(row=2, column=1, pady=20)
-
-
+################################################################################################
+# page des OF
 def page_of(root):
 
     def deconnexion():
         title_frame.place_forget()
         border.place_forget()
+        tableau.place_forget()
         page_login(root)
 
-    # Ajout d'une barre de titre en haut de la page
+    #Création du tableau
+    tableau = ttk.Treeview(root, columns=("Colonne 1", "Colonne 2", "Colonne 3", "Colonne 4", "Colonne 5","Colonne 6","Validation"),show="headings")
+
+    # Configurer les en-têtes de colonnes
+    tableau.heading("Colonne 1", text="N° OF")
+    tableau.heading("Colonne 2", text="Référence")
+    tableau.heading("Colonne 3", text="Quantité")
+    tableau.heading("Colonne 4", text="Date")
+    tableau.heading("Colonne 5", text="Heure")
+    tableau.heading("Colonne 6", text="Status")
+    tableau.heading("Validation", text="Validation")
+
+    # Insérer des données dans le tableau
+    for i in range(10):  # Par exemple, ajout de 10 lignes de données fictives
+
+        tableau.insert("", "end", values=(f"Ligne {i+1} - Colonne 1", f"Ligne {i+1} - Colonne 2", f"Ligne {i+1} - Colonne 3", f"Ligne {i+1} - Colonne 4", f"Ligne {i+1} - Colonne 5", f"Ligne {i+1} - Colonne 6", ""))
+        button_validation = tk.Button(tableau, text=f"Bouton {i+1}")
+        button_validation.place(relx=0.9, rely= i*0.01+0.02, relwidth=0.1, relheight=0.1)
+
+
+    # Placer le tableau dans la fenêtre principale
+    tableau.place(relx=0.1, rely=0.2, relwidth=0.8, relheight=0.7)
+
+        # Ajout d'une barre de titre en haut de la page
     border = tk.Frame(root, bg="black")
     border.place(relx=0, rely=0.11, relwidth=1, relheight=0.01)
 
@@ -57,9 +80,6 @@ def page_of(root):
     title_label.place(relx=0.4, rely=0.5, anchor="center")  # Centrer horizontalement
 
     # Ajout d'un bouton à droite du cadre
-    button_right = tk.Button(title_frame, text="Déconnexion", command= deconnexion)
+    button_right = tk.Button(title_frame, text="Déconnexion", command = deconnexion)
     button_right.place(relx=0.85, rely=0.5, anchor="center")  # Centrer horizontalement
-
-
-
 
