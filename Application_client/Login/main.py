7bd708cc84268a32f70d65385196edb24f06509d
Missivier <sivilier.alexandre@gmail.com>
2024-01-15 11:22:48 +0100

    integration

--- Application_client/Login/main.py
+++ Application_client/Login/main.py
@@ -1,3 +1,7 @@
+import sys
+sys.path.insert(0,'odoo')
+from intregration import ERP
+
 import tkinter as tk
 from tkinter import messagebox
 
--- odoo/intregration.py
+++ odoo/intregration.py
@@ -1,59 +1,65 @@
 import xmlrpc.client
 
 class ERP:
-    def __init__(self):
+    def __init__(self, db, username, password):
         self.odoo_ipaddr = "172.31.11.2"
         self.odoo_port = "8069"
         self.odoo_url = f'http://{self.odoo_ipaddr}:{self.odoo_port}'
-        self.db_name = 'db_cybervest'
-        self.username = 'enzo'
-        self.password = 'jslpdl'
+        self.db_name = db
+        self.username = username
+        self.password = password
         self.nom_article = []
         self.prix_article = []
         self.reference_interne = []
         self.stock_disponible = []
+        self.common = xmlrpc.client.ServerProxy(f'{self.odoo_url}/xmlrpc/2/common')
+        self.models = xmlrpc.client.ServerProxy(f'{self.odoo_url}/xmlrpc/2/object')
+        self.uid = self.connexion()
+
+    def connexion(self):
+         uid = self.common.authenticate(self.db_name, self.username, self.password, {})
+         if uid:
+            print('Connexion réussie. UID utilisateur:', uid)
+         else:
+            print('Échec de la connexion.')
+         return uid
 
     def obtenir_informations_produits(self):
-        common = xmlrpc.client.ServerProxy(f'{self.odoo_url}/xmlrpc/2/common')
-        uid = common.authenticate(self.db_name, self.username, self.password, {})
-
-        if uid:
-            models = xmlrpc.client.ServerProxy(f'{self.odoo_url}/xmlrpc/2/object')
-
-            product_ids = models.execute_kw(self.db_name, uid, self.password, 'product.product', 'search', [[]], {})
-            products = models.execute_kw(self.db_name, uid, self.password, 'product.product', 'read', [product_ids],
-                                        {'fields': ['name', 'list_price', 'default_code', 'qty_available']})
+         if self.uid:
+            product_ids = self.models.execute_kw(
+                self.db_name, self.uid, self.password,
+                'product.product', 'search', [[]], {}
+            )
+            products = self.models.execute_kw(
+                self.db_name, self.uid, self.password,
+                'product.product', 'read', [product_ids],
+                {'fields': ['name', 'list_price', 'default_code', 'qty_available']}
+            )
 
             for product in products:
                 self.nom_article.append(product['name'])
                 self.prix_article.append(product['list_price'])
                 self.reference_interne.append(product['default_code'])
                 self.stock_disponible.append(product['qty_available'])
-
-        else:
+         else:
             print('Échec de la connexion à Odoo.')
 
     def modifier_stock_odoo(self, default_code, new_stock):
-        common = xmlrpc.client.ServerProxy(f'{self.odoo_url}/xmlrpc/2/common')
-        uid = common.authenticate(self.db_name, self.username, self.password, {})
-
-        if uid:
-            models = xmlrpc.client.ServerProxy(f'{self.odoo_url}/xmlrpc/2/object')
-
-            product_id = models.execute_kw(
-                self.db_name, uid, self.password,
+        if self.uid:
+            product_id = self.models.execute_kw(
+                self.db_name, self.uid, self.password,
                 'product.product', 'search',
                 [[['default_code', '=', default_code]]]
             )
             if product_id:
-                quant_id = models.execute_kw(
-                    self.db_name, uid, self.password,
+                quant_id = self.models.execute_kw(
+                    self.db_name, self.uid, self.password,
                     'stock.quant', 'search',
                     [[['product_id', '=', product_id[0]]]]
                 )
                 if quant_id:
-                    models.execute_kw(
-                        self.db_name, uid, self.password,
+                    self.models.execute_kw(
+                        self.db_name, self.uid, self.password,
                         'stock.quant', 'write',
                         [quant_id, {'quantity': new_stock}]
                     )
@@ -62,20 +68,24 @@ class ERP:
                     print(f"Le produit avec le default_code '{default_code}' n'a pas de stock.")
             else:
                 print(f"Le default_code '{default_code}' n'a pas été trouvé.")
-
         else:
             print('Échec de la connexion à Odoo.')
 
     def afficher_variables(self):
-        print("Nom des articles :", self.nom_article[0])
-        print("Prix des articles :", self.prix_article[0])
-        print("Référence interne :", self.reference_interne[0])
-        print("Stock disponible :", self.stock_disponible[0])
+        if self.nom_article:
+            print("Nom des articles :", self.nom_article[0])
+        if self.prix_article:
+            print("Prix des articles :", self.prix_article[0])
+        if self.reference_interne:
+            print("Référence interne :", self.reference_interne[0])
+        if self.stock_disponible:
+            print("Stock disponible :", self.stock_disponible[0])
 
     def main(self):
+        self.connexion()
         self.obtenir_informations_produits()
         self.afficher_variables()
-        # Vous pouvez appeler self.modifier_stock_odoo() avec les valeurs nécessaires ici
+        self.modifier_stock_odoo()
 
     def run(self):
         self.main()
