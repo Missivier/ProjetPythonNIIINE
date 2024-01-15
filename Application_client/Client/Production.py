39fdae1a4beb2ce435f98edc36df83147d88d595
Valouze22 <valentin.ladrait@gmail.com>
2024-01-15 15:22:27 +0100

    modified variable com erp

--- Application_client/Client/Production.py
+++ Application_client/Client/Production.py
@@ -1,9 +1,8 @@
+from tkinter import Frame, Label, Button, ttk, Tk
 import tkinter as tk
-from tkinter import ttk, messagebox
 import sys
 sys.path.insert(0,'odoo')
 from intregration import ERP
-
 
 class BaseView(tk.Tk):
     """Classe de base pour les vues."""
@@ -14,49 +13,97 @@ class BaseView(tk.Tk):
         self.geometry(f"{self.screen_width}x{self.screen_height}+0+0")
 
 
-class ProductionPage(ttk.Frame):
-    def __init__(self, master=None):
-        super().__init__(master)
+class ProductionPage(Frame):
+    def __init__(self, master=None, **kwargs):
+        super().__init__(master, **kwargs)
+        self.label = Label(self, text="Production", font=('Helvetica', 24))
+        self.label.pack(pady=10)
 
-        # Données à afficher dans le tableau
-        data = [
-            ("A1", "B1", "C1"),
-            ("A2", "B2", "C2"),
-            ("A3", "B3", "C3")
-        ]
+        # Création de la grille pour afficher les articles
+        self.tree = ttk.Treeview(self, columns=("Numéro d'OF", "Date", "Quantité à réaliser", "Quantité en production"), show="headings")
+ 
+        # Configuration des en-têtes de colonnes
+        self.tree.heading("Numéro d'OF", text="Numéro d'OF", command=lambda: self.sort_column("Numéro d'OF", False))
+        self.tree.heading("Date", text="Date", command=lambda: self.sort_column("Date", False))
+        self.tree.heading("Quantité à réaliser", text="Quantité à réaliser", command=lambda: self.sort_column("Quantité à réaliser", False))
+        self.tree.heading("Quantité en production", text="Quantité en production", command=lambda: self.sort_column("Quantité en production", False))
+ 
+        # Ajout des colonnes avec une largeur augmentée de 50%
+        self.tree.column("Numéro d'OF", width=int(150 * 1.5), anchor="center")
+        self.tree.column("Date", width=int(100 * 1.5), anchor="center")
+        self.tree.column("Quantité à réaliser", width=int(100 * 1.5), anchor="center")
+        self.tree.column("Quantité en production", width=int(100 * 1.5), anchor="center")
 
-        # Création du tableau
-        self.tree = ttk.Treeview(self, columns=("Numéro d'OF", "Date", "Quantité produite"), show="headings")
+        self.tree.pack()
 
-        # Configuration des en-têtes du tableau
-        self.tree.heading("Numéro d'OF", text="Numéro d'OF")
-        self.tree.heading("Date", text="Date")
-        self.tree.heading("Quantité produite", text="Quantité produite")
+        # Ajout d'une instance de la classe ERP comme attribut de la classe HomeView
+        self.erp_instance = ERP()
 
-        # Ajout des données au tableau
-        for row in data:
-            self.tree.insert("", "end", values=row)
+        # Appeler la méthode pour obtenir les informations des produits et afficher le tableau
+        self.affichage_tableau()
 
-        # Liaison de l'événement de clic sur une ligne avec la fonction associée
-        self.tree.bind("<ButtonRelease-1>", self.on_item_click)
+        # Ajouter un bouton pour activer la modification du stock
+        self.modify_stock_button = Button(self, text="Modifier le stock", command=self.modif_stock)
+        self.modify_stock_button.pack(pady=10)
 
-        # Placement du tableau dans le cadre (Frame)
-        self.tree.pack(pady=10)
+    def affichage_tableau(self):
+        # Utiliser l'instance de la classe ERP
+        self.erp_instance.obtenir_informations_produits()
 
-    def on_item_click(self, event):
-        # Récupérer l'élément sélectionné
-        selected_item = self.tree.selection()
+        # Effacer les éléments existants dans la Treeview
+        for item in self.tree.get_children():
+            self.tree.delete(item)
 
-        if selected_item:
-            # Récupérer les valeurs de l'élément sélectionné
-            values = self.tree.item(selected_item, "values")
+        # Ajouter les nouvelles données obtenues à la Treeview
+        for i in range(len(self.erp_instance.nom_article)):
+            # Utiliser anchor pour centrer le texte
+            self.tree.insert("", "end", values=(self.erp_instance.nom_article[i], self.erp_instance.prix_article[i], 
+                                                self.erp_instance.reference_interne[i], self.erp_instance.stock_disponible[i]))
 
-            # Afficher une boîte de dialogue pour valider la quantité
-            result = messagebox.askquestion("Validation de la quantité", f"Valider la quantité produite ?", icon="question")
+    def update_table(self):
+        # Effacer les éléments existants dans la Treeview
+        for item in self.tree.get_children():
+            self.tree.delete(item)
 
-            # Supprimer la ligne si l'utilisateur clique sur "Valider"
-            if result == "yes":
-                self.tree.delete(selected_item)
+        # Ajouter les nouvelles données obtenues à la Treeview après mise à jour
+        for i in range(len(self.erp_instance.nom_article)):
+            self.tree.insert("", "end", values=(self.erp_instance.ordres[i], self.erp_instance.date[i], 
+                                                self.erp_instance.quantites[i], self.erp_instance.qty_producing[i]))
+
+    def sort_column(self, col, reverse):
+        # Obtenez les données actuelles de la Treeview
+        data = [(self.tree.set(child, "Nom"), self.tree.set(child, "Prix"), self.tree.set(child, "Référence Interne"), self.tree.set(child, "Stock Disponible"))
+                for child in self.tree.get_children("")]
+        
+        # Triez les données en fonction de la colonne spécifiée
+        data.sort(key=lambda x: x[int(col == "Prix")], reverse=reverse)
+        
+        # Effacez les éléments existants dans la Treeview
+        for item in self.tree.get_children():
+            self.tree.delete(item)
+        
+        # Ajoutez les données triées à la Treeview
+        for item in data:
+            self.tree.insert("", "end", values=item)
+
+    def modif_stock(self):
+        # Création du rectangle pour entrer le nombre d'articles
+        self.entry_frame = Tk.Frame(self.master)
+        self.entry_label = Tk.Label(self.entry_frame, text="Ajustement stock:")
+        self.entry_label.grid(row=0, column=0, padx=5, pady=5)
+ 
+        self.num_articles_entry = Tk.Entry(self.entry_frame)
+        self.num_articles_entry.grid(row=0, column=1, padx=5, pady=5)
+ 
+        # Création du bouton Valider
+        self.validate_button = Tk.Button(self.entry_frame, text="Valider", command=self.validate)
+        self.validate_button.grid(row=0, column=2, padx=5, pady=5, sticky="e")
+ 
+        self.entry_frame.grid(row=1, column=0, padx=10, pady=10, sticky="w")
+
+    def validate(self):
+        # Mettez ici le code pour valider et modifier le stock
+        pass
 
 
 if __name__ == "__main__":
