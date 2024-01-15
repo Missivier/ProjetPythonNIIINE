23faeab3960dd984aac35f4803e72cf72b3ef64e
MalCoMX29 <gabinmartingabin@gmail.com>
2024-01-15 15:27:14 +0100

    okok

--- odoo/intregration.py
+++ odoo/intregration.py
@@ -80,35 +80,17 @@ class ERP:
 
         return ordres_fabrication, dates_ordres_fabrication, quantite_a_produire, qty_producing
 
-    def afficher_variables(self):
-        if self.nom_article:
-            print("Nom des articles :", self.nom_article[0])
-        if self.prix_article:
-            print("Prix des articles :", self.prix_article[0])
-        if self.reference_interne:
-            print("Référence interne :", self.reference_interne[0])
-        if self.stock_disponible:
-            print("Stock disponible :", self.stock_disponible[0])
-
+   
     def main(self):
         self.connexion()
         self.obtenir_informations_produits()
-        self.afficher_variables()
+        self.obtenir_informations_ordres_fabrication()
+        
 
         # Obtention des informations des ordres de fabrication
         ordres, dates, quantites, qty_producing = self.obtenir_informations_ordres_fabrication()
-
-        # Vous pouvez utiliser ces valeurs comme nécessaire
-        print("Ordres de fabrication :", ordres)
-        print("Dates des ordres de fabrication :", dates)
-        print("Quantités à produire :", quantites)
-        print("Quantités en cours de production :", qty_producing)
 
         # Vous pouvez appeler self.modifier_stock_odoo() avec les valeurs nécessaires ici
 
     def run(self):
         self.main()
-
-if __name__ == "__main__":
-    erp_instance = ERP(db_name='db_cybervest', username='enzo', password='jslpdl')
-    erp_instance.run()
