43d85c91aedfa51c27f11760ff6edd1eb56a66ee
Valouze22 <valentin.ladrait@gmail.com>
2024-01-16 13:56:23 +0100

    modif admin page

--- Application_client/Client/Client.py
+++ Application_client/Client/Client.py
@@ -57,8 +57,13 @@ class Application(Tk):
         
         if self.erp.connexion( self.entry_username.get(), self.entry_password.get()) == 2 :
             self.pageProd()
+        elif self.erp.connexion( self.entry_username.get(), self.entry_password.get()) == 6:
+            self.page_admin_frame.place()
+            self.pageAdmin()
+
         else:
             self.pageLog()
+
 
     def pageProd(self):
 
@@ -137,7 +142,18 @@ class Application(Tk):
        # self.modify_stock_button = Button(self, text="Modifier", command=self.modif_stock)
         #self.modify_stock_button.pack(pady=10)
 
-
+    def pageAdmin(self):
+        #Création de la page
+        self.page_admin_frame = tk.Frame(self,bg="#DAD7D7")
+        #Creation bouton pour aller page prod
+        self.Button_prod = tk.Button(self.page_admin_frame, text="Production",fg="black", bg="#DAD7D7", font=("Arial", 20), command=self.show_prod_page_admin)
+        self.Button_prod.place(relx=0.3, rely=0.5, anchor="center")
+        #Creation bouton pour aller page logistique
+        self.Button_logis = tk.Button(self.page_admin_frame, text="Logistique",fg="black", bg="#DAD7D7", font=("Arial", 20), command=self.show_logis_page_admin)
+        self.Button_logis.place(relx=0.5, rely=0.5, anchor="center")
+        #Creation bouton pour aller page commerce
+        self.Button_commerce = tk.Button(self.page_admin_frame, text="Commerce",fg="black", bg="#DAD7D7", font=("Arial", 20),command=self.show_commerce_page_admin)
+        self.Button_commerce.place(relx=0.7, rely=0.5, anchor="center")
 
 
     def affichage_tableau(self):
