bc46924187170e09f7109f5cd9a1bdda4c9173b2
Valouze22 <valentin.ladrait@gmail.com>
2024-01-16 15:51:34 +0100

    modif admin

--- Application_client/Client/Client.py
+++ Application_client/Client/Client.py
@@ -206,6 +206,9 @@ class Application(Tk):
     def Retour(self):
         #Fonction pour revenir sur le menu admin
         self.Button_retour.place_forget()
+        self.page_prod_frame.place_forget()
+        self.page_log_frame.place_forget()
+        self.login_page()
 
 
     def affichage_tableau_log(self):
